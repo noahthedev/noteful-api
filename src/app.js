9fd06e8211dddf2fb645c70b0a074cbe48ab6fbc
Noah Roberts <noahmroberts@gmail.com>
2021-05-24 13:54:03 -0500

    updating package scripts

--- src/app.js
+++ src/app.js
@@ -4,12 +4,14 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const notesRouter = require('./notes/notes-router')
+const foldersRouter = require('./folders/folders-router')
 
 const app = express()
 
-const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common';
-
-app.use(morgan(morganOption))
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+  skip: () => NODE_ENV === 'test'
+}))
 app.use(helmet())
 app.use(cors())
 
--- src/notes/notes-service.js
+++
@@ -1,3 +0,0 @@
-const notesService = {
-  get
-}
